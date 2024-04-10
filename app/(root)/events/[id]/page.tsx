b3576d620a4184fa31e955953808adc81dc49cc8
kush2863 <70647570+kush2863@users.noreply.github.com>
2024-04-10 21:14:00 +0530

    done

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -73,7 +73,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
           <div className="flex flex-col gap-2">
             <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
             <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
-            <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">{event.url}</p>
+            
           </div>
         </div>
       </div>
